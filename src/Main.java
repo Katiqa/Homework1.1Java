f3e0d321ee4c33b16e18201fca1ea824ae06689a
Kati <katyushka.zykova.2015@mail.ru>
2023-05-23 19:24:51 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 12564; //цена билета
+        int mile = 20; //одна миля=кол-во руб
+        int bonus = price / mile;//вывод
+        System.out.println(bonus);
+    }
+}
